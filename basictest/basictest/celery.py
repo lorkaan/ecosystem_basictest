1eac5c8048a8f7ca4846d50b3747e8d7d9a1265a
Laurkan <laurkanr@gmail.com>
2026-07-28 12:53:05 -0400

    remove django.setup() from celery.py

--- basictest/basictest/celery.py
+++ basictest/basictest/celery.py
@@ -1,11 +1,14 @@
-from celery import Celery
 import os
+from celery import Celery
 
+# Set default Django settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "basictest.settings")
 
-import django
-django.setup() 
-
+# Create Celery app
 app = Celery("basictest")
+
+# Load config from Django settings (CELERY_ namespace)
 app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# Auto-discover tasks.py in installed apps
 app.autodiscover_tasks()
