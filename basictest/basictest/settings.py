c01846ec4c2b0d04dbffdbe8d98e26584294ef04
Laurkan <laurkanr@gmail.com>
2026-09-02 09:58:05 -0400

    added the 3000

--- basictest/basictest/settings.py
+++ basictest/basictest/settings.py
@@ -65,12 +65,12 @@ if TEST_FLAG:
     ]
 
     CORS_ALLOWED_ORIGINS = [
-        "http://localhost:5173",
+        "http://localhost:3000",
     ]
 
     CSRF_TRUSTED_ORIGINS = [
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
     ]
 else:
 
